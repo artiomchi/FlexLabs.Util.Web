06603a07805be05a00d286dbc4af59eb53e3b54d
Artiom Chilaru <artiom@flexlabs.org>
2017-02-19 14:13:50 +0000

    ! New tag helper was missing first and last pages

--- src/FlexLabs.Util.AspNetCore/TagHelpers/PagerTagHelper.cs
+++ src/FlexLabs.Util.AspNetCore/TagHelpers/PagerTagHelper.cs
@@ -11,30 +11,37 @@ namespace FlexLabs.AspNetCore.TagHelpers
         public override void Process(TagHelperContext context, TagHelperOutput output)
         {
             var pageData = new PagedListData(Model.PageItems);
-
             var inputName = ViewContext.ViewData.TemplateInfo.GetFullHtmlFieldName("Page");
+
+            if (!pageData.CanSeeFirstPage())
+                output.Content.AppendHtml(RenderPage(pageData, inputName, 1));
             foreach (var page in pageData.PageRange)
-            {
-                var liTag = new TagBuilder("li");
-                if (page == pageData.PageNumber)
-                {
-                    liTag.AddCssClass("page-current");
-                    liTag.InnerHtml.Append(page.ToString());
-                }
-                else
-                {
-                    var buttonTag = new TagBuilder("button");
-                    buttonTag.MergeAttribute("type", "submit");
-                    buttonTag.MergeAttribute("name", inputName);
-                    buttonTag.MergeAttribute("value", page.ToString());
-                    buttonTag.InnerHtml.Append(page.ToString());
-                    liTag.InnerHtml.AppendHtml(buttonTag);
-                }
-                output.Content.AppendHtml(liTag);
-            }
+                output.Content.AppendHtml(RenderPage(pageData, inputName, page));
+            if (!pageData.CanSeeLastPage())
+                output.Content.AppendHtml(RenderPage(pageData, inputName, pageData.PageCount));
 
             output.TagMode = TagMode.StartTagAndEndTag;
             output.TagName = "ul";
+        }
+
+        private TagBuilder RenderPage(PagedListData pageData, string inputName, int page)
+        {
+            var liTag = new TagBuilder("li");
+            if (page == pageData.PageNumber)
+            {
+                liTag.AddCssClass("page-current");
+                liTag.InnerHtml.Append(page.ToString());
+            }
+            else
+            {
+                var buttonTag = new TagBuilder("button");
+                buttonTag.MergeAttribute("type", "submit");
+                buttonTag.MergeAttribute("name", inputName);
+                buttonTag.MergeAttribute("value", page.ToString());
+                buttonTag.InnerHtml.Append(page.ToString());
+                liTag.InnerHtml.AppendHtml(buttonTag);
+            }
+            return liTag;
         }
     }
 }
